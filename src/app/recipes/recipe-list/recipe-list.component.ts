54a08f962adf2129102a6c24d82de1ea37c9fdbf
Michelle <mllaybolt@gmail.com>
2021-08-19 11:17:45 -0500

    Added dummy data and string interpolation for the recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes = [];
+recipes: Recipe[] = [
+  new Recipe('A Test Recipe', 'This is simply a test', 'https://dfbkuy5licyr9.cloudfront.net/wp-content/uploads/2021/04/iStock-1250703747.jpg'),
+  new Recipe('A Test Recipe', 'This is simply a test', 'https://dfbkuy5licyr9.cloudfront.net/wp-content/uploads/2021/04/iStock-1250703747.jpg'),
+]
   constructor() { }
 
   ngOnInit(): void {
