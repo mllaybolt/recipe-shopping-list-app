4c4156c10cfaabc7bb2e34aa07602f57da104e39
Michelle <mllaybolt@gmail.com>
2021-09-03 14:41:37 -0500

    Added a resolver to fix issue with data storage undefined after reload

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,36 +9,38 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Chicken Saltimbocca',
-      'Italian for "jump in your mouth"',
-      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-210212-chicken-saltimboca-002-ab-1617903467.jpg',
-      [
-        new Ingredient('Chicken Cutlets', 8),
-        new Ingredient('Proscuitto Slices', 16),
-        new Ingredient('White Wine', 1),
-        new Ingredient('Sage Leaves', 16),
-        new Ingredient('Salt', 1),
-        new Ingredient('Olive Oil', 3),
-      ]
-    ),
-    new Recipe(
-      'Lasagna Flatbread',
-      'A simple meal with classic Italian flavour',
-      'https://www.plainchicken.com/wp-content/uploads/2020/03/IMG_8231.JPG',
-      [
-        new Ingredient('Flatbread', 6),
-        new Ingredient('Ricotta Cheese', 1),
-        new Ingredient('Mozzarella Cheese', 1),
-        new Ingredient('Parmesan Cheese', 1),
-        new Ingredient('Egg', 1),
-        new Ingredient('Sausage', 1),
-        new Ingredient('Marinara Sauce', 1),
-        new Ingredient('Italian Seasoning', 1),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Chicken Saltimbocca',
+  //     'Italian for "jump in your mouth"',
+  //     'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-210212-chicken-saltimboca-002-ab-1617903467.jpg',
+  //     [
+  //       new Ingredient('Chicken Cutlets', 8),
+  //       new Ingredient('Proscuitto Slices', 16),
+  //       new Ingredient('White Wine', 1),
+  //       new Ingredient('Sage Leaves', 16),
+  //       new Ingredient('Salt', 1),
+  //       new Ingredient('Olive Oil', 3),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Lasagna Flatbread',
+  //     'A simple meal with classic Italian flavour',
+  //     'https://www.plainchicken.com/wp-content/uploads/2020/03/IMG_8231.JPG',
+  //     [
+  //       new Ingredient('Flatbread', 6),
+  //       new Ingredient('Ricotta Cheese', 1),
+  //       new Ingredient('Mozzarella Cheese', 1),
+  //       new Ingredient('Parmesan Cheese', 1),
+  //       new Ingredient('Egg', 1),
+  //       new Ingredient('Sausage', 1),
+  //       new Ingredient('Marinara Sauce', 1),
+  //       new Ingredient('Italian Seasoning', 1),
+  //     ]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -20,12 +21,22 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>(
         'https://ng-recipe-app-97aeb-default-rtdb.firebaseio.com/recipes.json'
       )
-      .subscribe((recipes) => {
-        this.recipeService.setRecipes(recipes);
-      });
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
